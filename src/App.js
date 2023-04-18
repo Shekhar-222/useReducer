5804cbb1210d2f75727e364157255aa10640f45e
Shekhar <shekhar30.shinde@gmail.com>
2023-04-18 19:23:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useReducer } from "react";
+
+let oldState = {
+  value: 0,
+};
+let reducerFunction = (oldState, action) => {
+  console.log("oldstate----->", oldState);
+  console.log("action----->", action);
+
+  switch (action.type) {
+    case "ADDITION":
+      return { ...oldState, value: oldState.value + action.payload };
+      break;
+    case "SUBSTRACRION":
+      return { ...oldState, value: oldState.value - action.payload };
+      break;
+    case "RESET":
+      return { ...oldState, value: 0 };
+      break;
+    default:
+  }
+};
 
 function App() {
+  const [newState, dispatch] = useReducer(reducerFunction, oldState);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{newState.value}</h1>
+      <button
+        onClick={() => {
+          dispatch({ type: "ADDITION", payload: 5 });
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "RESET" });
+        }}
+      >
+        Reset
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "SUBSTRACRION", payload: 5 });
+        }}
+      >
+        -
+      </button>
     </div>
   );
 }
